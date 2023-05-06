63d8ce605284c0f30345ae051103e7ab527fa633
IT20230692 <IT20230692@my.sliit.lk>
2023-05-06 08:46:30 +0530

    create product page and update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
-import Navbar from './components/Navbar/Navbar.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Shop from './container/OrderManagement/User/shop/Shop';
+import Cart from './container/OrderManagement/User/ShopingCart/Cart';
 import './App.css';
+import { ShopContextProvider } from './context/shop-context';
 
 const App = () => (
   <div>
-    <Navbar />
+  <ShopContextProvider>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </Router>
+    </ShopContextProvider>
   </div>
 );
 
